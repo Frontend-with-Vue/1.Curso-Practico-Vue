d41f888b633fdf7fdd418c6f97eb8f352ce27245
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-08-03 19:38:24 -0500

    fix(modalchoosedates): validation of correct dates

--- src/components/ModalChooseDates.vue
+++ src/components/ModalChooseDates.vue
@@ -6,21 +6,37 @@
     const endDate = ref('');
 
     function setRangeDates(){
+        if(!verifyDates()) {
+            filterDate.value = 3;
+            toggleModalChooseDate();
+            return;
+        };
         rangeDates.value = [startDate.value, endDate.value];
         filterDate.value = 2;
         toggleModalChooseDate();
-        console.log(rangeDates.value);
+    }
+
+    function verifyDates(){
+        if(!startDate.value) return false;
+        if(!endDate.value) return false;
+        if(new Date(startDate.value) > new Date(endDate.value)) return false;
+        return true;
+    }
+
+    function closeModal(){
+        filterDate.value = 3;
+        toggleModalChooseDate();
     }
 </script>
 
 <template>
     <section class="w-screen h-screen fixed z-20 top-0 left-0 grid place-content-center overflow-y-hidden">
-        <div @click="toggleModalChooseDate" class="absolute -z-0 w-full h-full bg-black opacity-35"></div>
+        <div @click="closeModal" class="absolute -z-0 w-full h-full bg-black opacity-35"></div>
         <div class="relative w-auto h-auto px-5 py-10 bg-white rounded-2xl shadow-[0_4px_25px_rgb(96,165,250)] ">
             <div class="flex justify-between gap-10 text-blue-400">
                 <h2 class="text-2xl">Rango de fechas</h2>
                 <button
-                    @click="toggleModalChooseDate"
+                    @click="closeModal"
                     class="text-2xl w-9 aspect-square rounded-full hover:bg-blue-400 hover:text-white transition">
                     <i class="bi bi-x-lg"></i>
                 </button>
