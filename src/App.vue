09232b4893d7348256e43860a958363a314e52c8
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-08-05 04:43:23 -0500

    feat: add routes and 404 not page found

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <script setup>
-import { defineAsyncComponent } from 'vue'
-import SplashScreen from './components/SplashScreen.vue'
-const AsyncHome =  defineAsyncComponent({
-  loader: () => new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(import('./components/Home-Cash.vue'));
-    }, 1000);
-  }),
-  loadingComponent: SplashScreen
-})
+import { ref, onMounted } from 'vue';
+import SplashScreen from './components/SplashScreen.vue';
+
+const loading = ref(true);
+// Simulacion de la carga con delay (testeo)
+const loadComponent = async () => {
+  loading.value = true;
+  await new Promise(resolve => setTimeout(resolve, 1500));
+  loading.value = false;
+};
+
+onMounted(() => {
+  loadComponent();
+});
 </script>
 
 <template>
-  <AsyncHome></AsyncHome>
+  <component :is="loading? SplashScreen: 'router-view'"></component>
+<!--
+  --Esta idea cuando los datos vengan del backend y demore
+  <Suspense>
+    <template #default>
+      <router-view />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense> -->
 </template>
 
 <style scoped>
---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    About page
+</template>
+
+
+<style scoped></style>
