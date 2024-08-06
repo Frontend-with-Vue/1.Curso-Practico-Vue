a0bcd4e7bdf8e4eb3d36e76a03d03d07cb9755d0
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-08-06 04:09:59 -0500

    feat: implement login

---
+++ src/views/LogIn.vue
@@ -0,0 +1,72 @@
+<script setup>
+    import { reactive, ref } from 'vue';
+    import { useRouter } from 'vue-router';
+
+    const userEmail = ref('');
+    const userPassword = ref('');
+
+    const router = useRouter();
+
+    const userCredentials = reactive([
+        {
+            email: '1@example.com',
+            password: 'password1'
+        },
+        {
+            email: '2@example.com',
+            password: 'password2'
+        },
+        {
+            email: '3@example.com',
+            password: 'password3'
+        }
+    ]);
+
+    function login(){
+        const user = userCredentials.find(user => 
+            user.email === userEmail.value && user.password === userPassword.value);
+        if(user){
+            router.push({
+                name: 'home',
+                query: {
+                    email: user.email,
+                    password: user.password
+                }
+            })
+        }
+    }
+</script>
+
+<template>
+    <main class="flex items-center min-h-screen bg-white dark:bg-gray-900">
+        <div class="container mx-auto">
+            <div class="max-w-md mx-auto my-10">
+                <div class="text-center">
+                    <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Sign in</h1>
+                    <p class="text-gray-500 dark:text-gray-400">Sign in to access your account</p>
+                </div>
+                <div class="m-7">
+                    <form @submit.prevent="login">
+                        <div class="mb-6">
+                            <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
+                            <input type="email" name="email" id="email" placeholder="you@company.com" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" v-model="userEmail"/>
+                        </div>
+                        <div class="mb-6">
+                            <div class="flex justify-between mb-2">
+                                <label for="password" class="text-sm text-gray-600 dark:text-gray-400">Password</label>
+                                <a href="#!" class="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300">Forgot password?</a>
+                            </div>
+                            <input type="password" name="password" id="password" placeholder="Your Password" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" v-model="userPassword"/>
+                        </div>
+                        <div class="mb-6">
+                            <button type="submit" class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">Sign in</button>
+                        </div>
+                        <p class="text-sm text-center text-gray-400">Don't have an account yet? <a href="#!" class="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800">Sign up</a>.</p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped></style>
