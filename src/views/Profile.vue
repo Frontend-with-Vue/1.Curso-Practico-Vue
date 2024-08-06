ad91fd0637a082712ff05c178a17293219e72d7e
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-08-06 07:29:10 -0500

    chore: implement props to route component

---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const props = defineProps({
+        userId: String
+    });
+</script>
+
+<template>
+    <h5>Profiles</h5>
+    <router-link :to="{name: 'profile', params: {userId: '1'}}">Profile 1</router-link>
+    <router-link :to="{name: 'profile', params: {userId: '2'}}">Profile 2</router-link>
+    <router-link :to="{name: 'profile', params: {userId: '3'}}">Profile 3</router-link>
+    <p>
+        {{ props.userId }}
+    </p>
+</template>
+
+<style scoped></style>
